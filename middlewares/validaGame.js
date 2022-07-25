6606bb78e18ca3ab8d76c9aef05ad99968d36b03
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-07-25 15:50:09 -0300

    middleware post game

--- middlewares/validaGame.js
+++ middlewares/validaGame.js
@@ -1,5 +1,11 @@
-
+import gameSchema from "../schemas/gameSchema.js";
 
 export function validaGame(req, res, next) {
-    
+    const jogo = req.body;
+    const validation = gameSchema.validate(jogo);
+    if (validation.error) {
+        return res.sendStatus(400);
+    }
+
+    next();
 }
