333a60d4bf180a95c8c5c6abbae441d67b96f8a9
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-07-25 15:59:49 -0300

    post game

--- controllers/gamesControllers.js
+++ controllers/gamesControllers.js
@@ -31,7 +31,27 @@ export async function getGames(req, res) {
 }
 
 export async function postGame(req,res) {
+    
+    const {categoryId, name, image, stockTotal, pricePerDay} = req.body;
 
+    try {
+        const result = await db.query(`SELECT id FROM categories WHERE id = $1`, [categoryId]);
+        if(result.rowCount === 0){
+            return res.sendStatus(400);
+        }
+
+        await db.query(`
+            INSERT INTO games(name, image, "stockTotal", "categoryId", "pricePerDay")
+            VALUES ($1, $2, $3, $4, $5)
+        `[name, image, Number(stockTotal), categoryId, pricePerDay]);
+
+        res.sendStatus(201);
+        
+        
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 
 
 }
