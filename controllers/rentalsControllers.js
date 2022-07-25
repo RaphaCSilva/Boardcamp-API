0ce388367f51a26a4a4b47cbcebb77453e18d03c
Raphacsilva <raphaelcavalcantesilva1999@gmail.com>
2022-07-25 17:45:42 -0300

    update rentals

--- controllers/rentalsControllers.js
+++ controllers/rentalsControllers.js
@@ -37,7 +37,50 @@ export async function getRentals(req, res) {
 }
 
 export async function postRental(req, res) { 
-    
+    const {customerId, gameId, daysRented} = req.body;
+
+    try {
+        const gameExist = await db.query(`
+            SELECT * FROM games WHERE id = $1
+        `,[gameId]);
+        if(gameExist === 0) {
+            return res.sendStatus(400);
+        }
+
+        const game = gameExist.rows[0];
+        const { pricePerDay } = game;
+
+        const costumerExist = await db.query(`
+            SELECT id FROM customers WHERE id = $1
+        `, [customerId]);
+        if(costumerExist.rowCount === 0) {
+            return res.sendStatus(400);
+        }
+
+        const result = await db.query(`
+            SELECT id FROM rentals WHERE "gameId" = $1 AND "returnDate" IS null
+        `, [gameId]);
+
+        if (result.rowCount > 0) {
+            if(game.stockTotal === result.rowCount) {
+                return res.sendStatus(400);
+            }
+        }
+
+        const Preco = daysRented * pricePerDay;
+
+        await db.query(`
+            INSERT INTO 
+                rentals ("customerId", "gameId", "rentDate", "dayRented", "returnDate", "originalPrice", "delayFee")
+                VALUES ($1, $2, NOW(), $3, null, $4, null);
+        `, [customerId, gameId, daysRented, Preco]);
+
+        res.sendStatus(201);
+
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
 }
 
 export async function updateRental(req, res) { 
@@ -49,12 +92,7 @@ export async function deleteRental(req, res) {
 }
 
 function arrumaArray(row) {
-    const [
-        id, customerId, gameId,
-        rentDate, daysRented, returnDate,
-        originalPrice, delayFee, customerName,
-        gameName, categoryId, categoryName
-    ] = row;
+    const [ id, customerId, gameId, rentDate, daysRented, returnDate, originalPrice, delayFee, customerName, gameName, categoryId, categoryName ] = row;
   
     return {
         id,
